09f5b7ec8b11175b21d51e9dcb26a9dd65d8753c
doblak <darjan@gmail.com>
2017-02-17 14:06:28 +0100

    linky string or null argument support

--- test/data/expected_testview.ts
+++ test/data/expected_testview.ts
@@ -26,7 +26,7 @@ interface Scope extends ng.IScope {
     }
 
 function ___f($scope: Scope, f__translate:(key: string) => string,
-    f__linky:(text:string, target: '_blank'|'_self'|'_parent'|'_top') => string,
+    f__linky:(text:string | null, target: '_blank'|'_self'|'_parent'|'_top') => string,
     f__orderBy:<T, K extends keyof T>(input:T[], field: K) => T[],
     f__filter:<T>(input:T[], v: string | { [P in keyof T]?: T[P]; }) => T[],
     f__limitTo:<T>(input: T[] | string | number, limit: string|number, begin?: string|number) => T[] | string,
