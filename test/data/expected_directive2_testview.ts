e8b73a33663db431b462a94efa84d367e1bed5cc
Emmanuel Touzery <etouzery@gmail.com>
2017-09-21 13:21:00 +0200

    0.0.31 quickfix over 0.0.30 change

--- test/data/expected_directive2_testview.ts
+++ test/data/expected_directive2_testview.ts
@@ -9,7 +9,7 @@ interface Scope extends ng.IScope {
 function ___f($scope: Scope, f__translate:(key: string) => string,
     f__linky:(text:string | null, target: '_blank'|'_self'|'_parent'|'_top') => string,
     f__orderBy:<T, K extends keyof T>(input:T[], field: K) => T[],
-    f__filter:<T>(input:T[], p: ((v:T,idx:number,array:T[])=>boolean | string | { [P in keyof T]?: T[P]; })) => T[],
+    f__filter:<T>(input:T[], p: (((v:T,idx:number,array:T[])=>boolean) | string | { [P in keyof T]?: T[P]; })) => T[],
     f__limitTo:<T>(input: T[] | string | number, limit: string|number, begin?: string|number) => T[] | string,
     f__date:(date:Date|string|number, format?: string, timezone?: string)=>string,
     f__formatNumber:(input: string, formatType: 'hex'|'dec') => string) {
